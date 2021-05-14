18f5f052d91db9b23f22ec5ef5f52a918d797f4c
Brandon Knox <bknox.dev@gmail.com>
2021-05-14 16:16:13 -0700

    Using a custom binding to get "new value is" to display properly.

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -10,18 +10,27 @@ import SwiftUI
 struct ContentView: View {
     @State private var blurAmount: CGFloat = 0 {
         didSet {
-            print("New value is \(blurAmount)")  // does not output
-            // property observer is not triggering
-            // need to add functionality to wrapped binding to work
+            print("New value is \(blurAmount)")
         }
     }
     
     var body: some View {
+        let blur = Binding<CGFloat>(
+            get: {
+                self.blurAmount
+            },
+                
+            set: {
+                self.blurAmount = $0
+                print("New value is \(self.blurAmount)")
+            }
+        )
+        
         VStack {
             Text("Hello, World!")
                 .blur(radius: blurAmount)
             
-            Slider(value: $blurAmount, in: 0...20)
+            Slider(value: blur, in: 0...20)
         }
     }
 }
