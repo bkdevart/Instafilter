40a651c60812a77d09e0ae3068d0d271c53fd5fa
Brandon Knox <bknox.dev@gmail.com>
2021-05-14 10:54:37 -0700

    Example of using blur effect on text with a slider

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,9 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var blurAmount: CGFloat = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, World!")
+                .blur(radius: blurAmount)
+            
+            Slider(value: $blurAmount, in: 0...20)
+        }
     }
 }
 
