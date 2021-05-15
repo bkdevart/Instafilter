40750812296683f82a99766fd3ffcd2cf4a114fc
Brandon Knox <bknox.dev@gmail.com>
2021-05-15 13:48:15 -0700

    CITwirlDistortion example

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -28,10 +28,11 @@ struct ContentView: View {
         let beginImage = CIImage(image: inputImage)
         
         let context = CIContext()
-        let currentFilter = CIFilter.crystallize()
+        guard let currentFilter = CIFilter(name: "CITwirlDistortion") else { return }
         
-        currentFilter.inputImage = beginImage
-        currentFilter.radius = 200
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        currentFilter.setValue(2000, forKey: kCIInputRadiusKey)
+        currentFilter.setValue(CIVector(x: inputImage.size.width / 2, y: inputImage.size.height / 2), forKey: kCIInputCenterKey)
         
         guard let outputImage = currentFilter.outputImage
         else { return }
